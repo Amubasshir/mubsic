e6b74e0df646792966051392b7725e1bd6a7587c
Mubasshir Ahmed <lastmubasshir@gmail.com>
2022-09-07 11:39:22 +0600

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 const App = () => {
-  return <div>Here we go....</div>;
+  return (
+    <div className=" App text-white overflow-hidden">
+      <Header />
+    </div>
+  );
 };
 
 export default App;
