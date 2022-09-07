8b70357d748420bf0f2e52ea1a28f0822d77c350
Mubasshir Ahmed <lastmubasshir@gmail.com>
2022-09-07 19:44:38 +0600

    search section added

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './App.css';
 import Experience from './components/Experience';
 import Header from './components/Header';
 import Hero from './components/Hero';
+import Search from './components/Search';
 const App = () => {
   return (
     <div className=" App text-white overflow-hidden">
       <Header />
       <Hero />
       <Experience />
+      <Search />
     </div>
   );
 };
